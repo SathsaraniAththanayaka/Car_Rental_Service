010f062a34c7a32779bc184abc3fd32ca6c66fc0
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2024-05-10 15:25:25 +0530

    Project added

---
+++ src/test/java/com/rentaroo/Car_Rental_Service/CarRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentaroo.Car_Rental_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
